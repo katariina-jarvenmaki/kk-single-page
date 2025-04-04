601d53dd7943d753f26c72b9dd446e110be9e6ea
Katariina Jarvenmaki <katariina.jarvenmaki@gmail.com>
2025-04-04 19:53:52 +0300

    Initial commit, forked base

---
+++ src/main/kotlin/com/katariinatuulia/com/kk_single_page/KkSinglePageApplication.kt
@@ -0,0 +1,11 @@
+package com.katariinatuulia.com.kk_single_page
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KkSinglePageApplication
+
+fun main(args: Array<String>) {
+	runApplication<KkSinglePageApplication>(*args)
+}
---
+++ src/test/kotlin/com/katariinatuulia/com/kk_single_page/KkSinglePageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.katariinatuulia.com.kk_single_page
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KkSinglePageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
